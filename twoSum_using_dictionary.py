9ee64fa9924766d2ccfb7a8377e647bc5bd5f9a0
olaniranolaolujames <olaniranolaolujames@gmail.com>
2016-02-17 22:15:34 +0100

    Delete twoSum_using_dictionary.py

--- twoSum_using_dictionary.py
+++
@@ -1,9 +0,0 @@
-def twoSum(nums, target):
-    index_dict = {}
-    list_sum = range(len(nums))
-    for i in list_sum:
-        both_diff = i - target
-        if both_diff not in index_dict:
-            index_dict[nums[i]] = i
-        else:
-            return [index_dict[both_diff], i]   
