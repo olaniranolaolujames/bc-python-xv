f65c76062fa6ab0c7e9ac9850c74a58e3e0d50c0
olaoluwa olaniran <olaoluwa olaniran>
2016-02-19 17:02:56 +0100

    NoteApplication Assignment

---
+++ assignments/notes_taking_app_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from notes_taking_app import *
+
+class NoteAppTestSuite(unittest.TestCase):
+
+    def test_create_note_content(self):
+        pass
+
+if __name__== "__main__":
+
+    unittest.main()
